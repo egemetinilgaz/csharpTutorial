64c89b32fa41db4fe7cd3652bfc42e22324fd4ef
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-07-30 21:25:38 +0300

    csharpHelloWorld

---
+++ helloWorld/helloWorld/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ege Metin ILGAZ ");
+            //console sinifinin, write komutu/metodu ekrana degeri yazar
+            //imlec oldugu yerde kalir
+            Console.WriteLine("Hello World");
+            //console sinifinin, writeline komutu/metodu ekrana degeri yazar
+            //imlec bir alt satira gecer
+            Console.ReadLine();
+            //kullanicidan deger alma komutudur
+        }
+    }
+}
+
+//bu bir yorum satiridir, bir satirdan olusur
+
+/*bu da bir yorum satiridir, birden daha fazla satirdan olusabilir
+ * 
+ * 
+ * 
+ */ 
+
+//yukaridaki using kismi vs tarafindan hazir gelen kutuphanedir
+
+//namespace uygulamanin tumunu kapsar, isim uzayidir
+
+//suslu parantezlere scope adi verilir, isim uzayinin
+//baslangic ve bitisini belirler
+
+//scope, icerisine tanimlayacagimiz islem bloklaridir
+
+//class yapilari, namespace in altinda classlar tanimlanir, .net tarafindan
+//hazir geldigi gibi kendi classlarimizi da yazabiliriz
+
+//main uygulamanin temel islem blogudur
