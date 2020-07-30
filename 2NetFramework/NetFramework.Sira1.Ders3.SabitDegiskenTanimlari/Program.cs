2bff2999bdeca89ba8312eb40445748954f430f8
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-07-30 21:34:06 +0300

    NetFramework.Sira1.Ders3.SabitDegiskenTanimlari

---
+++ 2NetFramework/NetFramework.Sira1.Ders3.SabitDegiskenTanimlari/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira1.Ders3.SabitDegiskenTanimlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+
+            sayi1 = 111;
+
+            const int ortakBolumDeger = 2; //const anahtar kelimesi ile sabit deger atadik
+            
+            int sonuc = sayi1 / ortakBolumDeger;
+
+            ortakBolumDeger = 3; //burada alinan hata ortakbolum bir sabittir ve sen buna deger atamasi yapamazsin
+
+           
+
+        }
+    }
+}
---
+++ 2NetFramework/NetFramework.Sira1.Ders3.SabitDegiskenTanimlari/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NetFramework.Sira1.Ders3.SabitDegiskenTanimlari")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NetFramework.Sira1.Ders3.SabitDegiskenTanimlari")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("39e5a216-5f09-437c-9d60-212f760169df")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
