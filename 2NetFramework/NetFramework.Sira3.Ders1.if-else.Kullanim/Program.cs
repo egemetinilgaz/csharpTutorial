dc09fc94bbc129f141b1eda4455ec909d6ea3118
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-08-15 10:33:07 +0300

    NetFramework.Sira3.Ders1.if-else.Kullanim

---
+++ 2NetFramework/NetFramework.Sira3.Ders1.if-else.Kullanim/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira3.Ders1.if_else.Kullanim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if-else genel kullanim
+            
+            if (true)
+            {
+                //dogru ise bu islemleri yap
+            }
+            else
+            {
+                //dogru degilse bu islemleri yap
+            }
+
+            //if - else baslangic
+
+            int sayi1 = 10;
+            int sayi2 = 15;
+
+            bool sonuc = sayi1 < sayi2;
+            if (sonuc)
+            {
+                Console.WriteLine("sayi1 - sayi2 degerinden kucuktur.");
+            }
+            else
+            {
+                Console.WriteLine("sayi1 - sayi2 degerinden buyuktur.");
+            }
+
+            //yukaridaki kodu daha kisa yazmanin yolu
+
+            if (sayi1 < sayi2)
+            {
+                Console.WriteLine("sayi1 - sayi2 degerinden kucuktur.");
+            }
+            else
+            {
+                Console.WriteLine("sayi1 - sayi2 degerinden buyuktur.");
+            }
+
+            //yukaridaki kodu daha kisa yazmanin yolu
+
+            if (sayi1 < sayi2) Console.WriteLine("sayi1 - sayi2 degerinden kucuktur.");
+            else Console.WriteLine("sayi1 - sayi2 degerinden buyuktur.");
+
+            //yukarida yazilan kodda if-else blogu tek bir islem/komut yapiyorsa
+            //suslu paranteze gerek yok.
+
+            Console.ReadLine();
+    
+        }
+    }
+}
