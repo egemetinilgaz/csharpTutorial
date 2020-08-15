526e90d7f9a3abe86f6daa8d827357bcac66fcfc
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-08-15 10:31:25 +0300

    NetFramework.Sira2.Ders5.RegionKullanimi

---
+++ 2NetFramework/NetFramework.Sira2.Ders5.RegionKullanimi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira2.Ders5.RegionKullanimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //region kullanimi 
+
+            #region ornek1
+
+
+
+            #endregion
+
+            #region ornek2
+
+
+
+            #endregion
+
+            //region, kodlari bloklamamiza yarar, kodlar daha duzenli durur
+            //region ile bloklanan kodlar derleyici tarafindan okunur ve calisir
+            //yorum satirlari gibi degillerdir, yorum satirlari text olarak saklanir ve okunmaz
+        }
+    }
+}
