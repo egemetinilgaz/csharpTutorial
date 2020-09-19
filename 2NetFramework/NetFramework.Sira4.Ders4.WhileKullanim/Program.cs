1b6abf4b6d144c26682dfea5712b51b77ce3eb80
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-09-20 00:39:35 +0300

    NetFramework.Sira4.Ders4.WhileKullanim

---
+++ 2NetFramework/NetFramework.Sira4.Ders4.WhileKullanim/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira4.Ders4.WhileKullanim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*while (true)
+            {
+            
+            }
+                /* while (true) //sartimiz true oldugu surece dongu calismasina dvam eder
+            
+                /*
+                 * 1- while (false) olana kadar devam eder
+                 * 2- dongu icerisinde bir yerde break kullanirsan donguden cikmis oluruz
+                 */
+
+            //ornek kullanim 1: 1-10 arasi sayilari yaz
+
+            //for ile yazdigim
+
+            /*for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Read();
+            Console.Clear();*/
+
+            //while ile yazdigim
+
+            int sayac = 1;
+
+            while (sayac <= 10)
+            {
+                Console.WriteLine(sayac);
+                sayac++;
+            }
+            Console.ReadLine();
+
+            //aslinda while dongusu bu islem icin kullanilmaz ama burada mantigi anlamak icin
+            //for dongusu kullanip bunu daha sonra while ile yaptim
+
+            //while dongusunun asil amaci bir silemin sonucu true olana kadar kullanmaktir
+        }
+    }
+}
+
