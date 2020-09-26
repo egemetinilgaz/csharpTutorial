ad2d2180f5ab657141a42ca1adf1b26d5196015c
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-09-26 16:46:39 +0300

    NetFramework.Sira4.Ders6.While.Quiz1

---
+++ 2NetFramework/NetFramework.Sira4.Ders6.While.Quiz1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira4.Ders6.While.Quiz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sistemin calisma zamaninda urettigi 1-10 arasindaki bir degeri
+            //kullanicinin tahmin etmesini iste, kac kez denedigini de bul ve ekrana yaz
+
+            //rnd = random
+
+            int sistemUretSayi = 0;
+            int tahminAdet = 1;
+            Random rnd = new Random();
+            sistemUretSayi = rnd.Next(1, 10); //next komutu uretmis oldugum sayiyi, degiskenime atamama yariyor
+
+            while (true)
+            {
+                Console.Write("{0}. deneme - sistemin uretmis oldugu sayiyi tahmin edin:", tahminAdet);
+                string rndKullaniciGelen = Console.ReadLine();
+                int rndKullaniciGelenInt = int.Parse(rndKullaniciGelen);
+
+                //if (int.Parse(rndKullaniciGelen) == sistemUretSayi) boyle kisa da yazilir
+
+                if (rndKullaniciGelenInt == sistemUretSayi)
+                {
+                    Console.WriteLine("{0}. denemede buldunuz, dogru sayi", tahminAdet);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("{0}. deneme, yanlis sayi, yeniden deneyin", tahminAdet);
+                    tahminAdet++;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
