fd852c7da27695b362148e5d4f192607bc3e14ac
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-09-27 09:36:07 +0300

    NetFramework.Sira4.Ders7.Do.While.Kullanim

---
+++ 2NetFramework/NetFramework.Sira4.Ders7.Do.While.Kullanim/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira4.Ders7.Do.While.Kullanim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //do while kullanim
+
+            do
+            {
+                //kesin 1 kere calisacak kisim
+            } while (true);
+
+            //do while dongulerinde sart ne olursa olsun, 1 kere kesin calisir
+        }
+    }
+}
