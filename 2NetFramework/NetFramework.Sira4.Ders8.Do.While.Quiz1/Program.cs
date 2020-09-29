08a136c781e1ba230b9f6ebee5324c5001480427
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-09-29 19:27:45 +0300

    NetFramework.Sira4.Ders8.Do.While.Quiz1

---
+++ 2NetFramework/NetFramework.Sira4.Ders8.Do.While.Quiz1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira4.Ders7.Do.While.Quiz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * kullaniciyi do while icerisine al ve kullanici adi sifre sor
+             * kullanici adi : Demo
+             * sifre : Demo
+             * dogru girerse donguden cik ve giris basarili yaz
+             * yanlis girerse kullanici adi ve sifre sormaya devam et
+             */
+
+            string kullaniciAdi = string.Empty;
+            string KullaniciSifre = string.Empty;
+            int sayac = 0;
+
+            do
+            {
+                /*
+                 * sayac ile kontrol onemli
+                 * do while icerisinde herhangi bir if kontrolu yapmadık
+                 * while (sart) blogu icinde kontrol yaptik
+                 * bu yuzden degerin yanlis veya dogru oldugunu sayac degiskeni ile kontrol ettik
+                 */
+
+                if (sayac > 0)
+                    Console.WriteLine("kullanici adi veya sifre hatali");
+                Console.WriteLine();
+
+                Console.WriteLine("kullanici adini girin: ");
+                kullaniciAdi = Console.ReadLine();
+
+                Console.WriteLine("sifre giriniz: ");
+                KullaniciSifre = Console.ReadLine();
+
+                sayac++;
+                //degerleri bir alt satirda kontrol ediyoruz
+            } while (kullaniciAdi.ToLower() != "demo" || KullaniciSifre.ToLower() != "demo");
+
+            //To.Lower: string e atanan tüm karakterleri kucuk harf yapar
+            //kullanici Demo - demo - DeMo yazsa da olacak
+
+            Console.WriteLine("basarili kullanici girisi");
+
+            Console.ReadLine();
+        }
+    }
+}
