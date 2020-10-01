d7b4e525d771b9ada11c7b3607d6941ac87a880d
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-10-01 20:32:15 +0300

    NetFramework.Sira4.Ders9.Do.While.Quiz2

---
+++ 2NetFramework/NetFramework.Sira4.Ders9.Do.While.Quiz2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira4.Ders8.Do.While.Quiz2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*kullanici 1 ile x arasinda bir sayi girsin
+             * daha sonra sistem kullanicinin girmis oldugu 1-x arasinda bir sayi tahmin etsin
+             * daha sonra kullanici sistemin tahmin etmis oldugu sayiyi bulsun
+             * bulamazsa yeniden sorsun
+             * kullanici ne zaman bulursa y kadar denediniz ve sonuca ulastiniz desin.
+             */
+
+            int odevTahmin = 0;
+            int odevUretilenSayi = 0;
+            int sayac = 0;
+
+            Console.WriteLine("tahmin icin bir sayi girin: ");
+            int odevKullaniciMaxValue = int.Parse(Console.ReadLine());
+            Random rnd = new Random();
+            odevUretilenSayi = rnd.Next(1, odevKullaniciMaxValue);
+
+            do
+            {
+                sayac++;
+                Console.WriteLine("{0}. deneme, uretilen sayiyi tahmin et", sayac);
+                odevTahmin = int.Parse(Console.ReadLine());
+
+            } while (odevTahmin != odevUretilenSayi);
+
+            Console.WriteLine("{0}. defa denediniz, sayiyi tahmin ettiniz, dogru sayi {1}", sayac, odevUretilenSayi);
+            Console.ReadLine();
+
+        }
+    }
+}
