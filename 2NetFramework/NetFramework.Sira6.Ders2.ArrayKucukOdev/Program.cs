4cb288385381ba028d4175c568897c3354b92fc5
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-10-05 20:27:02 +0300

    NetFramework.Sira6.Ders2.ArrayKucukOdev

---
+++ 2NetFramework/NetFramework.Sira6.Ders2.ArrayKucukOdev/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace NetFramework.Sira6.Ders2.ArrayKucukOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //10 elemanli (string) arraylist icindeki degerleri Z-A olacak sekilde sirala
+
+            ArrayList A1 = new ArrayList();
+
+            A1.Add("Metin");
+            A1.Add("Hatice");
+            A1.Add("Fatma");
+            A1.Add("Eda");
+            A1.Add("Rahmi");
+            A1.Add("Omer");
+            A1.Add("Zeynep");
+            A1.Add("Yagmur");
+            A1.Add("Yalcin");
+            A1.Add("Mehmet");
+
+            A1.Sort();
+            A1.Reverse();
+        }
+    }
+}
