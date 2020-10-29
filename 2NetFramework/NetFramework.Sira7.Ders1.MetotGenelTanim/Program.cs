ec06add3fc67c1a53e114d8c5e406e1b2b4157b4
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-10-29 10:02:50 +0300

    NetFramework.Sira7.Ders1.MetotGenelTanim

---
+++ 2NetFramework/NetFramework.Sira7.Ders1.MetotGenelTanim/Program.cs
@@ -0,0 +1,53 @@
+﻿using System; //bir kutuphane dosyasini uygulama icirisine ekler
+using System.Collections.Generic; //kutuphane
+using System.Linq; //kutuphane
+using System.Text; //kutuphane
+using System.Threading.Tasks; //kutuphane
+
+namespace NetFramework.Sira7.Ders1.MetotGenelTanim
+{
+    class Program
+    {
+        static void Main(string[] args) //burasi metot, main metodu
+        {
+            //yukaridaki using.system kutuphanesini kullanmadan uzun yazalim
+            System.Console.Write("1hello");
+            //kutuphaneyi kullandigimizda da boyle olur
+            Console.Write("2hello");
+            Console.WriteLine("");
+
+            //yukaridaki 5 kutuphanenin ozellikle neden eklenmis oldugunu ogrenelim
+            /*
+             * gelen kutuphanaler .net framework versiyonlari ile degisir
+             * yenilendikce guncellenir, kolaylik saglar
+             */
+
+            //namespace kismi kutuphane adini icerir, isim uzayidir
+            //baska br uygulamada kutuphane olarak kullanabilirim
+            //namespace icerisinde class tanimlari bulunur
+
+            //class .net icerisinde olusturdugumuz, icerisinde metotlari ve 
+            //bir cok ozellikleri barindiran kucuk paketlerdir
+
+            //bir classin icinde birden fazla  metot olur
+            //bu metotlarlar cagirildikca is yapar
+
+            //main metodu uygulamanin ilk baslayacagi kisimdir
+            //ilk scope araligidir, su an bunlari yazdigim aralik
+
+            //void, metot calisacak ama geriye parametre(deger) donmeyecek demek
+
+            //SayHello();
+
+            ogrenci O1 = new ogrenci(); //ogreci sinifindaki metoda ulasmak icin yazdik
+            //O1.sayHelloStudent();
+            O1.ogrenciMetot2();
+        }
+
+        static void SayHello() //parantez bos oldugu icin deger almaz
+        {
+            Console.WriteLine("3hello");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2NetFramework/NetFramework.Sira7.Ders1.MetotGenelTanim/ogrenci.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira7.Ders1.MetotGenelTanim
+{
+    public class ogrenci
+    {
+        public void sayHelloStudent()
+        {
+            Console.WriteLine("say hello student :)");
+            Console.ReadLine();
+
+        }
+
+        /* metot olusturmak icin  gerekli seyler
+             * [erisim belirleyicisi] <geri donus degeri> metotadi (parametreler)
+             * {
+             *    //metodun  calismasi icin gereken kodlarr
+             * }
+             * 
+             * erisim belirleyicisi
+             * 
+             * public //genelolan, herkesin erisebildigi
+             * private //ozel olan, herkesin erisemedigi
+             * protected //birbirinden nesne kalitirsa ulasirsin
+             * internal //ilgili kutuphane  icinde ulasirsin
+             * internal protected //ilgili kutuphaneden veya bir yerden kalitildiysa ulasirsin 
+             *
+             */
+
+        private void ogrenciMetot1(string ogrendiAdi, string ogrenciSoyadi)
+        {
+            Console.WriteLine("ogrenci bilgileri:{0} {1}", ogrendiAdi, ogrenciSoyadi);
+            Console.ReadLine();
+        }
+
+        public void ogrenciMetot2()
+        {
+            ogrenciMetot1("Ege Metin", "ILGAZ");
+            Console.ReadLine();
+        }
+    }
+}
