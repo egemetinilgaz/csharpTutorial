dc654eb587bdc8443748c6ac47abc599b91008d2
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-10-30 20:29:15 +0300

    NetFramework.Sira7.Ders2.Matematiksel-Islemler

---
+++ 2NetFramework/NetFramework.Sira7.Ders2.Matematiksel-Islemler/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira7.Ders2.Matematiksel_Islemler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //kullanicidan 2 deger alip 4 islem yaptir,  metot ile yap
+
+            matematik M = new matematik();
+        yenidenIslemYap: //burasi bir lable
+        yenidenSecim: //kullanici hatali deger girerse buraya donecek
+
+            M.menuHazirla();
+            int kullaniciSecim = int.Parse(Console.ReadLine());
+
+            if (kullaniciSecim > 4 || kullaniciSecim < 1)
+            {
+                Console.WriteLine("hatali deger girisi yaptiniz");
+                Console.WriteLine("1-4 araliginda yeniden secim yapin ");
+                System.Threading.Thread.Sleep(3000);
+                goto yenidenSecim;
+            }
+
+            Console.WriteLine("birinci sayiyi girin: ");
+            decimal kullaniciSayi1 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("ikinci sayiyi girin: ");
+            decimal kullaniciSayi2 = decimal.Parse(Console.ReadLine());
+
+            decimal sonuc = 0;
+
+            switch (kullaniciSecim)
+            {
+                case 1: //toplama islemi
+                    sonuc = M.toplamaIslemi(kullaniciSayi1, kullaniciSayi2);
+                    M.sonucEkranaYaz(kullaniciSayi1, "+", kullaniciSayi2, sonuc);
+                    break;
+                case 2: //cikartma islemi
+                    sonuc = M.cikartmaIslemi(kullaniciSayi1, kullaniciSayi2);
+                    M.sonucEkranaYaz(kullaniciSayi1, "-", kullaniciSayi2, sonuc);
+                    break;
+                case 3: //carpma islemi
+                    sonuc = M.carpmaIslemi(kullaniciSayi1, kullaniciSayi2);
+                    M.sonucEkranaYaz(kullaniciSayi1, "*", kullaniciSayi2, sonuc);
+                    break;
+                case 4: //bolme islemi
+                    sonuc = M.bolmeIslemi(kullaniciSayi1, kullaniciSayi2);
+                    M.sonucEkranaYaz(kullaniciSayi1, "/", kullaniciSayi2, sonuc);
+                    break;
+                //hocanin vermis oldugu ve cevapladigi bu odevde
+                //program yukaridaki if dongusu olmadan default alanina girmiyor
+                //nedenini anlamadim ve yukariya kendim if blogu koydum
+                default:
+                    Console.WriteLine("girmis oldugunuz deger, liste icinde bulunamadi");
+                    Console.WriteLine("yeniden bir deger girin");
+                    System.Threading.Thread.Sleep(2000);
+                    goto yenidenIslemYap; //burada kullanici farkli bir 
+                    //deger girerse basa donmesini sagliyoruz
+            }
+            //burasi do while ile de yapilablirdi
+            Console.WriteLine("yeni bir islemyapmak ister misiniz ? (Y/N) ");
+            string YN = Console.ReadLine();
+            if (YN.ToUpper() == "Y")
+            {
+                goto yenidenIslemYap; //tepedeki lable a gider
+            }
+        }
+    }
+}
---
+++ 2NetFramework/NetFramework.Sira7.Ders2.Matematiksel-Islemler/matematik.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira7.Ders2.Matematiksel_Islemler
+{
+    public class matematik
+    {
+        //topla metodu yaz
+        public decimal toplamaIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 + sayi2;
+            return sonuc;
+        }
+
+        //cikar metodu yaz
+        public decimal cikartmaIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 - sayi2;
+            return sonuc;
+        }
+
+        //carp metodu yaz
+        public decimal carpmaIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 * sayi2;
+            return sonuc;
+        }
+
+        //bolme metodu yaz
+        public decimal bolmeIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 / sayi2;
+            return sonuc;
+        }
+
+        //menu metodu yap
+
+        public void menuHazirla()
+        {
+            Console.Clear();
+            Console.WriteLine("***menu***");
+            Console.WriteLine("1- toplama");
+            Console.WriteLine("2- cikartma");
+            Console.WriteLine("3- carpma");
+            Console.WriteLine("4- bolme");
+            Console.Write("islem icin bir deger secin: ");
+        }
+
+        //sonucu ekrana yazma metodu
+
+        public void sonucEkranaYaz(decimal kullaniciSayi1, string operators,
+            decimal kullaniciSayi2, decimal sonuc)
+        {
+            Console.WriteLine("{0} {1} {2} = {3}", kullaniciSayi1, operators, kullaniciSayi2, sonuc);
+        }
+    }
+}
