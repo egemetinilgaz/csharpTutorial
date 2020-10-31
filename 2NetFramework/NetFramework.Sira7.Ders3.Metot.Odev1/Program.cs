4e26f1a1b19b01d5d95a5188b43427d0ec1709b2
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-10-31 16:38:46 +0300

    NetFramework.Sira7.Ders3.Metot.Odev1

---
+++ 2NetFramework/NetFramework.Sira7.Ders3.Metot.Odev1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira7.Ders3.Metot.Odev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * kullanicidan ogrenci adi, soyadi ve 3 tane ders notu al
+             * ogrenci sinifi icinde ogrencipuanhesapla adinda bir metoda parametre olarak ver
+             * ekrana yaz
+             * ortalama>=45 gecer, ortalama<45 kalir
+             */
+
+            string ogrenciAd = string.Empty;
+            string ogrenciSoyad = string.Empty;
+            decimal not1 = 0;
+            decimal not2 = 0;
+            decimal not3 = 0;
+            //decimal ortalama = 0;
+
+            Console.WriteLine("ortalmasini hesaplamak  istediginiz ogrenci bilgilerini yazin");
+
+            Console.Write("ad: ");
+            ogrenciAd = Console.ReadLine();
+
+            Console.Write("soyad:");
+            ogrenciSoyad = Console.ReadLine();
+
+            Console.Write("not1: ");
+            not1 = decimal.Parse(Console.ReadLine());
+
+            Console.Write("not2: ");
+            not2 = decimal.Parse(Console.ReadLine());
+
+            Console.Write("not3: ");
+            not3 = decimal.Parse(Console.ReadLine());
+
+            ogrenci O = new ogrenci();
+            //not3 degeri ogrenci cs kisminda default olarak 45 ayarlandigi icin burada
+            //not3 kismi diger not1 ve not2 parametrelerinden farkli gorunecek
+            //burada soyle oluyor, eger not3 e bir not girerse ogrenci onu aliyor
+            //not3 e not girilmezse default degeri olan 45 i aliyor
+            O.ogrenciNotHesapla(ogrenciAd, ogrenciSoyad, not1, not2, not3);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2NetFramework/NetFramework.Sira7.Ders3.Metot.Odev1/ogrenci.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira7.Ders3.Metot.Odev1
+{
+    public class ogrenci
+    {
+        //parametreler 2 ye ayrilir opsiyonel (dolmasi zorunlu olmayan)
+        //opsiyonel olmayan(dolmasi zorunlu olan)
+        public void ogrenciNotHesapla(string ogrenciAdi, string OgrenciSoyadi, decimal ogNot1,
+            decimal ogNot2, decimal ogNot3 = 45) //opsiyonel parametreler en sonda yazilmalidir
+            //aralara yazildiginda program hata verir, ya da opsiyonel kullandigin parametreden sonra
+            //bir parametre yazarsan =0 diyebilirsin
+        //yukarida ogrencinin ogNot3 un girisi zorunlu ama biz bunu opsiyonlu bir hale getiriyoruz
+        //bu sekilde deger girilmedigi zaman da default degeri 45 olacak sekilde calisacak
+        //bu sekilde yapilir "decimal ogNot3 = 45"
+        {
+            Console.WriteLine("ogrenci bilgileri");
+            Console.WriteLine("ad soyad: {0} {1}", ogrenciAdi, OgrenciSoyadi);
+            //burada ortalamayi asagidaki private metodu cagirar aldim
+            decimal ortalama = ortalamaHesapla(ogNot1, ogNot2, ogNot3);
+            //bu kisim benden ogrenciOrtalama adinda bir parametre istemiyor
+            //sadece decimal bir veri tipi olan parametre istiyor
+            ortalamaNotDegerlendir(ortalama);
+
+            //yukaridaki kisimda yine ortalama alindiktan sonra bir islem yapilmadigi icin
+            //kisa yazim sekli
+            // ortalamaNotDegerlendir(ortalamaHesapla(ogNot1, ogNot2, ogNot3));
+            //yukaridaki gibi ic ice olan metotlarda once ic kisim calisir
+        }
+        void ortalamaNotDegerlendir(decimal ogreniOrtalama)
+        {
+            if (ogreniOrtalama < 45)
+            {
+                Console.WriteLine("ortalama: {0} ile kaldiniz", ogreniOrtalama);
+            }
+            else
+            {
+                Console.WriteLine("ortalama {0} ile gectiniz", ogreniOrtalama);
+            }
+        }
+        //parametre degerlerinin isimleri ayni olmak zorunda degil
+        //ortalamaHesapla da ogrencinot1 kullanirken, ogrenciNotHesapla da ogNot1 kullandim
+        private decimal ortalamaHesapla(decimal ogrencinot1, decimal ogrencinot2, decimal ogrencinot3)
+        {
+            decimal ortalama1 = (ogrencinot1 + ogrencinot2 + ogrencinot3) / 3;
+            return ortalama1;
+
+            //yukarida ortalama alindiktan sonra baska bir islem yapilmadigi icin
+            //decimal ortalama1 diye br degisken tanimlayip hafizada tutmaya gerek yok
+            //bunu asagidaki gibi kisa bir sekilde de yazabiliriz
+            // return (ogrencinot1 + ogrencinot2 + ogrencinot3) / 3;
+        }
+    }
+}
