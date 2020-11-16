cd94fc879d5f1d2daf628ac489c89c6c2cf646d3
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-11-16 19:52:15 +0300

    NetFramework.Sira7.Ders8.MetotLocalFunction

---
+++ 2NetFramework/NetFramework.Sira7.Ders8.MetotLocalFunction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira7.Ders8.MetotLocalFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //metot local function
+            localFunctionKullanimi(11, 22);
+            Console.ReadLine();
+        }
+
+        static void localFunctionKullanimi(int sayi1, int sayi2)
+        {
+            // int toplam = sayi1 + sayi2; normalde boyle yazardik
+
+            //buraya ozel bir metot tasarlayacagim ve toplama islemini burada yapacagim
+            //metot icerisindeki islemler sadece o metotda kullanilirsa localfunction yaparsin
+
+            int topla(int gelenSayi1, int gelenSayi2)
+            {
+                return gelenSayi1 + gelenSayi2;
+            }
+
+            int toplamDeger = topla(sayi1, sayi2);
+
+            Console.WriteLine(toplamDeger);
+        }
+    }
+}
