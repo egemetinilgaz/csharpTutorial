fe9ad025bf696172db238d50071fe35105bc4242
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-11-22 14:35:14 +0300

    NetFramework.Sira8.Ders2.Siniflarda.Metot

---
+++ 2NetFramework/NetFramework.Sira8.Ders2.Siniflarda.Metot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira8.Ders2.Siniflarda.Metot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            musteri M1 = new musteri();
+
+            M1.TCKimlikNo = "12345678912";
+            M1.isim = "Ege Metin";
+            M1.soyisim = "ILGAZ";
+            M1.cinsiyet = 71777001;
+
+            musteri M2 = M1; //burada bir referans verdim, M1 nereyi gosteriyorsa sen de oraya bak dedim
+
+            M2.isim = "Hatice"; //M1'de de deger degisir
+            M1.TCKimlikNo = "98765432198"; //M2'de de deger degisir
+
+            bool musteriKontrolEt = M1.musteriKontrol();
+        }
+    }
+}
---
+++ 2NetFramework/NetFramework.Sira8.Ders2.Siniflarda.Metot/musteri.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira8.Ders2.Siniflarda.Metot
+{
+    internal class musteri
+    {
+        internal musteri() //bu bir yapici metot constructor
+        {
+
+        }
+
+        internal musteri(string _tckimlikno) //bu bir yapici metot constructor
+        {
+            TCKimlikNo = _tckimlikno;
+        }
+
+        internal musteri(string _tckimlikno, string _isim) //bu bir yapici metot constructor
+        {
+            TCKimlikNo = _tckimlikno;
+            isim = _isim;
+        }
+
+        internal musteri(string _tckimlikno, string _isim, string _soyisim) //bu bir yapici metot constructor
+        {
+            TCKimlikNo = _tckimlikno;
+            isim = _isim;
+            soyisim = _soyisim;
+        }
+
+        internal string TCKimlikNo;
+        internal string isim;
+        internal string soyisim;
+        internal int cinsiyet; //ornek 717770001: erkek, 717770002: kadin
+
+        internal bool musteriKontrol()
+        {
+            bool kontrol = musteriKontrolDatabase(TCKimlikNo);
+            return kontrol;
+        }
+
+        private bool musteriKontrolDatabase(string _tckimlikno)
+        {
+            //database e gidilip, musterinin kimlik numarasina gore daha once
+            //kayit edilip edilmedigine bakilir ve biz buna simdilik true diyoruz
+            return true;
+        }
+    }
+}
