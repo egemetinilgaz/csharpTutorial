1c4bc9cec59e3ad64d88f9cb9c22dce3394c7c17
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-11-28 16:27:10 +0300

    NetFramework.Sira9.Ders1.Encapsulation

---
+++ 2NetFramework/NetFramework.Sira9.Ders1.Encapsulation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira9.Ders1.Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            musteri Musteri1 = new musteri();
+            Musteri1.id = 442;
+            Musteri1.isim = "Ege Metin";
+            Musteri1.soyisim = "ILGAZ";
+
+            //encapsulation: class inicindeki field'a giden datanin, gitme ve 
+            //o dataya ulasma bolumlerini detaylandirma kismina denir
+            
+            /*
+             * kapsulleme (encapsulation)
+             * field icerisine deger atanamasin ama deger okunsun
+             * field icerisine deger atamasi yapilsin ama okunamasin
+             * field icerisine deger atansin ama degerinilk4 hanesi okunsun
+             * 1-field icerisine deger atama ve okuma yapilsin ama belirlenen bir surectengecsin
+             * 1-ve veri sonra gosterilsin
+             */
+
+            //class icinde bir alanin deger alma ve  deger okuma kisimlarina encapsulation denir
+
+            //property, get, set
+
+            // Musteri1.emailAdres = "ilgazzegemetin@gmail.com"; (private field oldugu icin hata verir)
+
+            //burasi property, field degil
+            //genel olarak encapsulation konusuna giris yaptik
+            Musteri1.EMailAdress = "ilgazzegemetin@gmail.com";
+            Console.Write(Musteri1.EMailAdress);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2NetFramework/NetFramework.Sira9.Ders1.Encapsulation/musteri.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira9.Ders1.Encapsulation
+{
+    internal class musteri
+    {
+        // internal; NetFramework.Sira9.Ders1.Encapsulation icinde erisilebilen demektir
+
+        //burasi class'in field'i yani alani
+        internal int id;
+        internal string isim;
+        internal string soyisim;
+
+        private string emailAdres; //burasi private dis dunya goremez, burasi private field
+
+        //dis dunyaya public olacak, aldigi degeri ic dunyayaki emailadres'e gonderecek
+        //class icindeki ozellikler yani property
+
+        //buradaki email yukaridakinden farkli, karistirma
+        public string EMailAdress
+        {
+            //get; //database veya farkli veri kaynagindan data cektigimizde, 
+            //class cagiran programciya datayi gosterdigimiz kisim
+            //set; //dis dunyadan cektigim datayi, private olarak sakladigim field icerisine deger
+            //atandigim kisim (private string emailAdres;) bu kisim
+
+            get
+            {
+                return this.emailAdres; //burada da onu cagiran kisiye gonderiyoruz
+            }
+
+            set
+            {
+                this.emailAdres = value;
+                //gelen degeri(ilgazzegemetin@gmail.com) value anahtar kelimesi ile yakalariz 
+                //ve emailAdres e atariz
+                //buradaki value uzerinde istedigimiz islemi yapariz
+                //metotda gonderirirm
+                //dogrulugunu kontrol ederim 
+                //if else bloguna sokarim
+                //vs
+            }
+        }
+    }
+}
