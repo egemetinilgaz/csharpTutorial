17abde9c3088cddd9cdd13f44072843945a68b7b
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-11-29 12:05:01 +0300

    NetFramework.Sira9.Ders2.Encapsulation.Quiz1

---
+++ 2NetFramework/NetFramework.Sira9.Ders2.Encapsulation.Quiz1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira9.Ders2.Encapsulation.Quiz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bir onceki encapsulation dersinde yaptigimiz
+            //get edilmesin ama set edilsin orneginin tam tersini
+            //farkli icerikli bir ornek ile sen yap
+
+            aile benimAilem = new aile();
+
+            Console.WriteLine("aile kisi sayisi:" + benimAilem.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2NetFramework/NetFramework.Sira9.Ders2.Encapsulation.Quiz1/aile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira9.Ders2.Encapsulation.Quiz1
+{
+    internal class aile
+    {
+        //bir onceki encapsulation dersinde yaptigimiz
+        //get edilmesin ama set edilsin orneginin tam tersini
+        //farkli icerikli bir ornek ile sen yap
+
+        public aile()
+        {
+            this.sayi = aileKacKisi();
+        }
+
+
+
+        private int sayi;
+
+        public int aileBirey
+        {
+            private get
+            {
+                return this.aileBirey;
+            }
+            set
+            {
+                this.aileBirey = value;
+            }
+        }
+
+
+
+        private int aileKacKisi()
+        {
+            int aileSayi = 5;
+            return aileSayi;
+        }
+    }
+}
