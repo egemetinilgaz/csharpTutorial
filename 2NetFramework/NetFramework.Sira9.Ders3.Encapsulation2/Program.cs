dfb91de18861cefbe709e79c42b4cab272d0b7d6
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-12-02 21:56:20 +0300

    NetFramework.Sira9.Ders3.Encapsulation2

---
+++ 2NetFramework/NetFramework.Sira9.Ders3.Encapsulation2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira9.Ders2.Encapsulation2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            musteri2 Musteri1 = new musteri2();
+
+            
+            Musteri1.EMailAdress = "ilgazzegemetin@gmail.com";
+            Console.WriteLine(Musteri1.EMailAdress);
+
+
+
+
+            //musteri2 class'indaki public int ID'yi cagirdik
+            //get ile istedigimiz degeri aldik
+            //set'i de private yaptik, ve mudahale edilemez hale getirdik
+            //field icerisine deger atanamasin ama deger okunsun
+            Console.WriteLine("musteri id numarasi: " + Musteri1.ID.ToString());
+            
+            // Musteri1.ID = 10; //burada hata verecek, cunku set blogu private
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2NetFramework/NetFramework.Sira9.Ders3.Encapsulation2/musteri2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira9.Ders2.Encapsulation2
+{
+    internal class musteri2
+    {
+        //burasi class'in field'i yani alani
+        
+        internal string isim;
+        internal string soyisim;
+        //emailAdres asagida private
+        //id asagida private
+
+        //yapici metot olusturuyoruz
+        //nesnem orneklendiginde, yapici metodum calisacak
+        //yapici metodum iceride private olan IDUret metodundan random bir deger alacak
+        //aldigi bu random degeri, yine iceride private olan id uzerine atacak
+        //daha sonra da get ve set ile, private id uzerinden islem yapacagim
+        public musteri2()
+        {
+            this.id = IDUret();
+        }
+
+
+
+
+
+        //get ve set metotlari public'tir bunu unutma
+        //field icerisine deger atanamasin ama deger okunsun
+        private int id;
+        public int ID
+        {
+            get
+            {
+                return this.id;
+            }
+            private set //burada ID'ye mudahale edilmesin istiyorsan, ID'yi private yapacaksin
+            {
+                this.id = value;
+            }
+        }
+
+        private int IDUret()
+        {
+            Random rnd = new Random();
+            return rnd.Next(10000, 90000);
+        }
+
+
+
+
+
+        private string emailAdres; //burasi private field dis dunya goremez
+        public string EMailAdress
+        {
+            //get; //database veya farkli veri kaynagindan data cektigimizde, 
+            //class cagiran programciya datayi gosterdigimiz kisim
+            //set; //dis dunyadan cektigim datayi, private olarak sakladigim field icerisine deger
+            //atandigim kisim (private string emailAdres;) bu kisim
+
+            set
+            {
+                this.emailAdres = value;
+                
+            }
+
+            get
+            {
+                return this.emailAdres;
+            }
+        }
+    }
+}
