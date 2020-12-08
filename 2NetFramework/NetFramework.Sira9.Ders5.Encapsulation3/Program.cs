f7651cb5f9a7d50acb9f3872da64e3e827e4109e
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-12-08 22:54:11 +0300

    NetFramework.Sira9.Ders5.Encapsulation3

---
+++ 2NetFramework/NetFramework.Sira9.Ders5.Encapsulation3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira9.Ders3.Encapsulation3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            email eMail1 = new email();
+
+            eMail1.isim = "egemetin";
+            eMail1.SoyIsim = "ilgaz";
+            Console.WriteLine(eMail1.EMailAdres);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2NetFramework/NetFramework.Sira9.Ders5.Encapsulation3/email.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira9.Ders3.Encapsulation3
+{
+    internal class email
+    {
+        public string isim;
+
+        private string _soyisim;
+
+        public string SoyIsim
+        {
+            get
+            {
+                return this._soyisim;
+            }
+            set
+            {
+                this._soyisim = value;
+                this.emailAdres = string.Format("{0} {1}@gmail.com", isim, _soyisim);
+            }
+        }
+
+        private string emailAdres;
+
+        public string EMailAdres
+        {
+            get
+            {
+                return this.emailAdres;
+            }
+            private set
+            {
+                this.emailAdres = value;
+            }
+        }
+
+
+    }
+}
