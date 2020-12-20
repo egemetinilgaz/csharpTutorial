366a005c6376b8e8f4501575738eeaf619e12cac
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-12-20 11:41:50 +0300

    NetFramework.Sira10.Ders2.StaticField

---
+++ 2NetFramework/NetFramework.Sira10.Ders2.StaticField/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira10.Ders2.StaticField
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            personel P1 = new personel();
+            P1.isim = "Ege Metin";
+            P1.soyisim = "ILGAZ";
+            P1.EMailAdres = "egemetinilgaz";
+            P1.cinsiyet = 71777001; //71777002 kadin icin
+        }
+    }
+}
