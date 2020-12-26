6e5442a58f36b52d4aea61cf72c21db85cfc27c6
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2020-12-27 00:16:02 +0300

    NetFramework.Sira10.Ders4.StaticClass

---
+++ 2NetFramework/NetFramework.Sira10.Ders4.StaticClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira10.Ders4.StaticClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            personel P1 = new personel();
+            P1.isim = "Ege Metin";
+            P1.soyisim = "ILGAZ";
+            P1.EMailAdres = "egemetinilgaz";
+            P1.cinsiyet = 71777001; //71777002 kadin icin
+
+            helper.emailGoner("iem@firmam.com", "yeni personel girisi", P1.isim + P1.soyisim);
+            //yukarida ilk cagirdiginda ekran ciktisi asagidaki gibidir
+            // helper => static class => static CTOR
+            // mail gonderme islemi basarili
+            // yani static yapici metot calisti ve isi bitti
+
+            helper.emailGoner("iem@firmam.com", "yeni personel girisi", P1.isim + P1.soyisim);
+            //burada ise tekrar cagirilmasina ragmen calismaz ve cikti asagidaki gibi olur
+            // mail gonderme islemi basarili
+        }
+    }
+}
---
+++ 2NetFramework/NetFramework.Sira10.Ders4.StaticClass/helper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira10.Ders4.StaticClass
+{
+    public static class helper //artik static class haline geldi
+                               //artik static helper'in program cs tarafinda nesnesi alinamaz
+                               //kendi icinde ise sadece static field ve static metot barindirir
+                               //asagida static olmayan bir sey kullanamazsin
+    {
+        /*public helper()
+        {
+           //her nesne ornegi alindiginda, uygulama standart yapici metodu calistirir
+        }*/
+
+
+
+        //burasi static yapici metot
+        static helper() //burada static olarak, yapici bir metot olusturduk, bunlar private olurlar
+        {
+            //static yapici metotlar, uygulama boyunca bir defa calisir(ilk cagirilma)
+            //tekrar cagirdiginda calismazlar
+            Console.WriteLine("helper => static class => static CTOR");
+        }
+
+
+        //burasi normal static metot
+        public static void emailGoner(string aliciEmailAdres, string konu, string icerik)
+        {
+            //email gonderme islemi devam edecek.(burada bir simulasyon yaptim)
+            Console.WriteLine("mail gonderme islemi basarili");
+        }
+
+        /* 
+         * static olmayan bir class icinde
+         * 1 static ctor
+         * 1 normal ctor oldugunda
+         * yine ne kadar orneklenirse orneklensin
+         * static ctor yalnizca 1 kere calisir
+         * 
+         */
+
+
+    }
+}
