778acd6d2b43993c1e57874b39299c6297445c68
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2021-01-01 14:45:11 +0300

    NetFramework.Sira10.Ders5.BigQuiz1

---
+++ 2NetFramework/NetFramework.Sira10.Ders5.BigQuiz1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira10.Ders5.BigQuiz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * musteri adinda bir class olustur
+             * bu nesnenin icine static olarak bir arraylist olustur
+             * musteri sinifinin icerisine;
+             * int: id
+             * string: isim, soyisim, emailadres, kullaniciadi, sifre field'lari olustur
+             * olusturdugumuz bu field listesinden bazilarinin encapsulation filtreleri;
+             * kullanici adi degeri sanal olarak olusturdugumuz database icerisinde aransin
+             * eger bu deger database icinde adi degeri varsa, 
+             * field icerisine eklenmek istenen deger eklenmesin(encapsulation)
+             * musteri ekle adinda bir static metot olusturalim
+             * parametre olarak musteri tipi alsin
+             * musteri nesnesinin null olup olmadigini
+             * musteri nesnesi icerisindeki kullanici adinin dolu olmasi sartina baksin
+             * yukarida kriterlerden gecerse sanal olarak olusturdugumuz database'imize eklenmeden once
+             * email adresinden br kontrol saglansin
+             * eger sistemde ayni email adresi ile bir musteri degeri varsa eklenmesin
+             * yoksa sanal database icerisine ilgili nesneyi ekleyelim
+             */
+
+            musteri M1 = new musteri();
+            M1.MusteriID = 1;
+            M1.isim = "Ege Metin";
+            M1.soyisim = "ILGAZ";
+            M1.eMailAdres = "ilgazzegemetin@gmail.com";
+            M1.kullaniciAdi = "EgeMetinILGAZ";
+            M1.sifre = "12345";
+
+            musteri.MusteriEkle(M1);
+
+
+
+            //boyle bir yazim sekli de var
+            musteri M2 = new musteri()
+            {
+                MusteriID = 2,
+                isim = "Hatice",
+                soyisim = "ILGAZ",
+                eMailAdres = "ilgazhatice@gmail.com",
+                kullaniciAdi = "haticeilgaz",
+                sifre = "54321"
+            };
+
+            musteri.MusteriEkle(M2);
+
+
+
+            //buradaki amac bilerek ayni girmek ve sistemin hata vermesini saglamak
+            musteri M3 = new musteri();
+            M3.MusteriID = 1;
+            M3.isim = "Ege Metin";
+            M3.soyisim = "ILGAZ";
+            M3.eMailAdres = "ilgazzegemetin@gmail.com";
+            M3.kullaniciAdi = "EgeMetinILGAZ";
+            M3.sifre = "12345";
+
+            musteri.MusteriEkle(M3);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2NetFramework/NetFramework.Sira10.Ders5.BigQuiz1/musteri.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira10.Ders5.BigQuiz1
+{
+    internal class musteri
+    {
+        #region SANAL DATABASE
+        static ArrayList musteriDatabese;
+        #endregion
+
+        #region STATIC YAPICI METOT
+
+        static musteri() //static yapici metot
+        {
+            musteriDatabese = new ArrayList();
+        }
+
+        #endregion
+
+        #region FIELD
+
+        public int MusteriID { get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+        public string eMailAdres { get; set; }
+        public string sifre { get; set; }
+
+
+
+        private string _kullaniciAdi;
+
+        #endregion
+
+        #region ENCAPSULATION
+
+        public string kullaniciAdi
+        {
+            get
+            {
+                return this._kullaniciAdi;
+            }
+            set
+            {
+                bool kullaniciAdiKontrol = musteriKullaniciAdiKontol(value);
+                if (kullaniciAdiKontrol)
+                {
+                    Console.WriteLine("bu kullanici adi sistemde kayitli");
+                    this._kullaniciAdi = string.Empty;
+                }
+                else
+                {
+                    this._kullaniciAdi = value;
+                }
+            }
+        }
+
+        #endregion
+
+        #region STATIC METHODS
+
+        static bool musteriKullaniciAdiKontol(string kuladi)
+        {
+            bool kontrol = false;
+
+            for (int i = 0; i < musteriDatabese.Count; i++)
+            {
+                musteri temp = (musteri)musteriDatabese[i];
+                // soyle de yazilir ama daha karisik
+                //if(((musteri)musteriDatabese[i]).kullaniciAdi == _kullaniciadi)
+
+                //arraylist obje sakladigi icin zorunlu tip donusumu yapiyoruz
+                //obje olarak gelen degeri, cevirip temp uzerine aliyoruz
+                //gecici olarak, yine musteri class'inin ismi ile sadece kontrol amacli actigimiz
+                //musteri temp = (musteri) uzerine atiyoruz
+                if (temp.kullaniciAdi == kuladi)
+                {
+                    kontrol = true;
+                    break;
+                }
+            }
+
+            return kontrol;
+        }
+        static bool musteriEmailAdresKontrol(string kulemailadres)
+        {
+            bool kontrol = false;
+            for (int i = 0; i < musteriDatabese.Count; i++)
+            {
+                musteri temp = (musteri)musteriDatabese[i];
+                //arraylist obje sakladigi icin zorunlu tip donusumu yapiyoruz
+                //obje olarak gelen degeri, cevirip temp uzerine aliyoruz
+                //gecici olarak, yine musteri class'inin ismi ile sadece kontrol amacli actigimiz
+                //musteri temp = (musteri) uzerine atiyoruz
+                if (temp.eMailAdres == kulemailadres)
+                {
+                    kontrol = true;
+                    break;
+                }
+            }
+            return kontrol;
+        }
+        #endregion
+
+
+
+        #region MUSTERI EKLE METOT
+        public static void MusteriEkle(musteri musterigirisi)
+        {
+            #region PARAMETERS CONTROLS
+            if (musterigirisi != null && !string.IsNullOrEmpty(musterigirisi.kullaniciAdi) && !string.IsNullOrEmpty(musterigirisi.eMailAdres))
+            //m.kullaniciAdi null ya da bos degilse
+            {
+                bool emailAdresKontrol = musteriEmailAdresKontrol(musterigirisi.eMailAdres);
+                if (emailAdresKontrol)
+                {
+                    Console.WriteLine("eklemek istediginiz kullanici sistemde kayitli");
+                }
+                else
+                {
+                    musteriDatabese.Add(musterigirisi);
+                    Console.WriteLine("yeni kayit islemi basarili");
+                }
+            }
+            else
+            {
+
+            }
+            #endregion
+        }
+        #endregion
+    }
+}
