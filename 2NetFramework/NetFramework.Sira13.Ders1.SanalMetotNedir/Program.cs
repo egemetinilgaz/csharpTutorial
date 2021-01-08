a329eabe08fb84e09ecc3431db2c41d2f73896cb
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2021-01-08 23:29:37 +0300

    NetFramework.Sira13.Ders1.SanalMetotNedir

---
+++ 2NetFramework/NetFramework.Sira13.Ders1.SanalMetotNedir/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira13.Ders1.SanalMetotNedir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //butun nesneler object'den gelir
+
+            //tostring metodu cagirildiginda, 
+            //cagrilan class'in, namespace'ini ve class'in adini bize doner
+            //bu .net'in standart davranisi
+
+            musteri M1 = new musteri();
+            M1.isim = "Ege Metin";
+            M1.soyisim = "ILGAZ";
+            string toStringMesaj = M1.ToString();
+            Console.WriteLine(toStringMesaj);
+            Console.ReadLine();
+
+            //ekran  ciktisi: NetFramework.Sira13.Ders1.SanalMetotNedir.musteri
+        }
+    }
+}
---
+++ 2NetFramework/NetFramework.Sira13.Ders1.SanalMetotNedir/musteri.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira13.Ders1.SanalMetotNedir
+{
+    public class musteri // :object => framework standart olarak tum nesneleri object'den turetir
+    {
+        public int musteriID { get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+
+        public override string ToString()
+        {
+            // return base.ToString(); - standart kullanim
+            return isim + " " + soyisim;
+            //bunu yaptigimiz zaman object nesnesi icindeki ToString() metodu calismayacak
+            //benim musteri nesnesi icindeki ToString() metodum calisacak
+            //sanalmetot oldugu icin mudahale ettigimiz metot calisir
+        }
+    }
+}
