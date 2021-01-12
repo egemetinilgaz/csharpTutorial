6db17af9971e161d1e5ad070db439a73d2af7724
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2021-01-12 20:17:59 +0300

    NetFramework.Sira15.Ders1.InterfaceNedir

---
+++ 2NetFramework/NetFramework.Sira15.Ders1.InterfaceNedir/Imusteri.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira15.Ders1.InterfaceNedir
+{
+    public interface Imusteri
+    {
+        /*public Imusteri()
+        {
+
+        }*/
+        //interface icerisinde yapici metot kullanilmaz
+
+        /*
+         * interface icerisinde neler olur
+         * field
+         * metot
+         * delegate (henuz gormedi bunu, ileride gorecegiz)
+         * .
+         * .
+         */
+
+        //field
+
+        int ID { get; set; }
+        string isim { get; set; }
+        string soyisim { get; set; }
+
+        //metot
+
+        int yeniKayit(string isim, string soyisim);
+        int kayitDuzenle(int id, string isim, string soyisim);
+        int kayitSil(int id);
+    }
+}
---
+++ 2NetFramework/NetFramework.Sira15.Ders1.InterfaceNedir/musteri.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira15.Ders1.InterfaceNedir
+{
+    public class musteri : Imusteri //implement interface dediginde hata vermeyecek (sari ampule tikla)
+    {
+        /*
+        public int ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string isim { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string soyisim { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int kayitDuzenle(int id, string isim, string soyisim)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int kayitSil(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int yeniKayit(string isim, string soyisim)
+        {
+            throw new NotImplementedException();
+        }*/
+        //yukaridaki kisim hazir gelir, bunu sen dolduraaksin
+
+        int _id;
+        string _isim;
+        string _soyisim;
+        //asagidaki prop'lar degiskenlere ihtiyac duyduklari icin bu degiskenleri once yaziyoruz
+
+        public int ID { get { return -ID; } set { _id = value; } }
+        public string isim { get { return _isim; } set { _isim = value; } }
+        public string soyisim { get { return _soyisim; } set { _soyisim = value; } }
+
+        public int kayitDuzenle(int id, string isim, string soyisim)
+        {
+            Console.WriteLine("kayit duzenlendi");
+            return 1;
+        }
+
+        public int kayitSil(int id)
+        {
+            Console.WriteLine("kayit silindi");
+            return 1;
+        }
+
+        public int yeniKayit(string isim, string soyisim)
+        {
+            Console.WriteLine("yeni kayit eklendi");
+            return 2;
+        }
+    }
+}
