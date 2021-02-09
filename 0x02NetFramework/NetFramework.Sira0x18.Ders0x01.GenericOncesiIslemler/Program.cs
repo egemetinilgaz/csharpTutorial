2dec7a4ce3eb63e52166f923617730b85a5d71c3
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2021-02-09 22:25:58 +0300

    NetFramework.Sira0x18.Ders0x01.GenericOncesiIslemler

---
+++ 0x02NetFramework/NetFramework.Sira0x18.Ders0x01.GenericOncesiIslemler/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira18.Ders1.GenericOncesiIslemler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //generic oncesi islemler boyle yapiliyordu
+
+            //hatirlatma arraylist obje turunden saklar
+
+            /*
+             * ArrayList liste = new ArrayList();
+             * liste.Add()
+             * 
+             */
+
+            sanalDatabase sDatabase = new sanalDatabase();
+            {
+                sDatabase.yeniKayit(1); //burada sadece int girebiirsin
+            }
+
+            /*
+             * yukaridaki amacimiz sanalDatabase class'i icerisinde private bir metot
+             * olusturup, bu metot icerisinden istedigimiz veri tipinde bir veri tipi
+             * kullanilmasini zorunlu kilmak
+             */
+        }
+    }
+}
---
+++ 0x02NetFramework/NetFramework.Sira0x18.Ders0x01.GenericOncesiIslemler/sanalDatabase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.Sira18.Ders1.GenericOncesiIslemler
+{
+    public class sanalDatabase
+    {
+        private ArrayList listem;
+        public sanalDatabase()
+        {
+            listem = new ArrayList();
+        }
+
+        public void yeniKayit(int data)
+        {
+            listem.Add(data);
+        }
+
+        //ben burada private olarak sakladigim arraylist'ime, istemis oldugum, zorunlu kostugum
+        //veri tipini aldim. yani sadece int alabilir.
+    }
+}
