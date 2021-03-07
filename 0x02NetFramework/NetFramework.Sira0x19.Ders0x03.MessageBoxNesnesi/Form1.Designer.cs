b46276ae18e0028fbbd480305c57611034af87df
Ege Metin ILGAZ <59531182+egemetinilgaz@users.noreply.github.com>
2021-03-07 15:22:23 +0300

    NetFramework.Sira0x19.Ders0x03.MessageBoxNesnesi

---
+++ 0x02NetFramework/NetFramework.Sira0x19.Ders0x03.MessageBoxNesnesi/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace NetFramework.Sira19.Ders3.MessageBoxNesnesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMesajGoster = new System.Windows.Forms.Button();
+            this.btnSoruSor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMesajGoster
+            // 
+            this.btnMesajGoster.Location = new System.Drawing.Point(12, 12);
+            this.btnMesajGoster.Name = "btnMesajGoster";
+            this.btnMesajGoster.Size = new System.Drawing.Size(151, 85);
+            this.btnMesajGoster.TabIndex = 0;
+            this.btnMesajGoster.Text = "Mesaj Goster";
+            this.btnMesajGoster.UseVisualStyleBackColor = true;
+            this.btnMesajGoster.Click += new System.EventHandler(this.btnMesajGoster_Click);
+            // 
+            // btnSoruSor
+            // 
+            this.btnSoruSor.Location = new System.Drawing.Point(169, 12);
+            this.btnSoruSor.Name = "btnSoruSor";
+            this.btnSoruSor.Size = new System.Drawing.Size(151, 85);
+            this.btnSoruSor.TabIndex = 0;
+            this.btnSoruSor.Text = "Soru Sor";
+            this.btnSoruSor.UseVisualStyleBackColor = true;
+            this.btnSoruSor.Click += new System.EventHandler(this.btnSoruSor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 364);
+            this.Controls.Add(this.btnSoruSor);
+            this.Controls.Add(this.btnMesajGoster);
+            this.Name = "Form1";
+            this.Text = "Message Box Nesnesi Inceleme";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMesajGoster;
+        private System.Windows.Forms.Button btnSoruSor;
+    }
+}
+
---
+++ 0x02NetFramework/NetFramework.Sira0x19.Ders0x03.MessageBoxNesnesi/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetFramework.Sira19.Ders3.MessageBoxNesnesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMesajGoster_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("kullaniciya gostermek istedigimiz bilgi", "baslik bilgisi",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnSoruSor_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("yeniden denemek ister misin?",
+                 "soru", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+
+            if(res == DialogResult.Yes)
+            {
+
+            }
+            else if (res == DialogResult.No)
+            {
+
+            }
+            else if(res == DialogResult.Cancel)
+            {
+
+            }
+        }
+    }
+}
